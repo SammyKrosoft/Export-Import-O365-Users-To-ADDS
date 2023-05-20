8f475a4957576a969512a4a4dbc7c2fe9c9cf5c5
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2023-05-19 23:49:05 -0400

    Update ImportUsersToAD.ps1
    updated, working now (error in splatting parameters with hashtable ... use @ sign instead of $ sign ... bruh)

--- ImportUsersToAD.ps1
+++ ImportUsersToAD.ps1
@@ -1,15 +1,52 @@
+############################# PUT YOUR OWN VALUES HERE #####################################
+
 # Setup your environment variables/parameters such as domain name, etc...
 ## your domain name to replace to make the UPN
 $YourDomain = "@yourdomain.com"
 ## Distinguished name of the destination OU for your groups:
-$GroupsDestOU = "OU=Groups,DC=Contoso,DC=com"
+$DestOU = "OU=CANADADREY Users,DC=CANADADREY,DC=LOCAL"
 ## export directory for the CSV file - DO NOT PUT BACKSLASH AT THE END
-$ExportDirectory = "c:\temp"
+$ExportDirectory = "c:\temp" # NO BACKSLASH AT THE END OF THE DIR. 
+$CSVImportFile = "O365UserData.csv"
+
+############################################################################################
+
 
 # import Users
-$UsersToImport = import-csv "$ExportDirectory\o365userdata.csv" -Encoding UTF8
+$UsersToImport = import-csv "$ExportDirectory\$CSVImportFile" -Encoding UTF8
+
 
 Foreach ($user in $UsersToImport) {
-     New-ADUser -Path ("OU="+$_.Department+","+$GroupDestOU) -Name $_.Name -SamAccountName $_.SAMAccountName -GivenName $_.FirstName -Surname $_.LastName -Department $_.Department -DisplayName $_.DisplayName -EmailAddress $_.EmailAddress -Office $_.Office -ChangePasswordAtLogon $True -Title $_.Title -UserPrincipalName $_.UserPrincipalName -Enable $True -AccountPassword (ConvertTo-SecureString -string $_.Password -AsPlainText -force)
+
+#$HashTable = @{}
+
+
+$HashTable = @{
+    Path = "$DestOU"
+    Enabled = $True
+    ChangePasswordAtLogon = $True
+    }
+
+
+If (-not ([string]::IsNullOrEmpty($user.Name))) {$HashTable.Add('Name',$user.Name)}
+If (-not ([string]::IsNullOrEmpty($user.SamAccountName))) {$HashTable.Add('SamAccountName',$user.SamAccountName)}
+If (-not ([string]::IsNullOrEmpty($user.FirstName))) {$HashTable.Add('GivenName',$user.FirstName)}
+If (-not ([string]::IsNullOrEmpty($user.LastName))) {$HashTable.Add('Surname',$user.LastName)}
+If (-not ([string]::IsNullOrEmpty($user.Department))) {$HashTable.Add('Department',$user.Department)}
+If (-not ([string]::IsNullOrEmpty($user.DisplayName))) {$HashTable.Add('DisplayName',$user.DisplayName)}
+If (-not ([string]::IsNullOrEmpty($user.EmailAddress))) {$HashTable.Add('EmailAddress',$user.EmailAddress)}
+If (-not ([string]::IsNullOrEmpty($user.Office))) {$HashTable.Add('Office',$user.Office)}
+If (-not ([string]::IsNullOrEmpty($user.Title))) {$HashTable.Add('Title',$user.Title)}
+If (-not ([string]::IsNullOrEmpty($user.UserPrincipalName))) {$HashTable.Add('UserPrincipalName',$user.UserPrincipalName)}
+If (-not ([string]::IsNullOrEmpty($user.Password))) {$HashTable.Add('AccountPassword',$(ConvertTo-SecureString -string $user.Password -AsPlainText -force))}
+
+$HashTable
+
+
+    New-ADUser @HashTable -Verbose -Debug -Confirm:$false
+
+
+
      }
+ 
  
